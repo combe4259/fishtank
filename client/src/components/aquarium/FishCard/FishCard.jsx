b39a0f5ac5a4ca1c67c4007a5cf26539ceed66f5
combe4259 <82356080+combe4259@users.noreply.github.com>
2025-07-15 14:31:00 +0900

    Update FishCard.jsx

--- client/src/components/aquarium/FishCard/FishCard.jsx
+++ client/src/components/aquarium/FishCard/FishCard.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Gem, Plus, Heart } from 'lucide-react';
-import { styles } from './fishCard-styles';
+import { styles } from './fishcard-styles';
 
 const FishCard = ({ fish, onClick, showPrice = false, fishImage, isOwned = false }) => {
     const [isHovered, setIsHovered] = useState(false);
